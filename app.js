bd07e94e98f31eb5cf3fe973756209499cabfc5d
Dallen Pyrah <dallenpyrah@gmail.com>
2021-02-10 09:27:09 -0700

    main changes

---
+++ app.js
@@ -0,0 +1,13 @@
+
+function oddOrEven(num){
+    if(num % 2 == 0){
+        return "even"
+    }else{
+        return "odd"
+    }
+}
+console.log(oddOrEven(1))
+console.log(oddOrEven(2))
+console.log(oddOrEven(3))
+console.log(oddOrEven(4))
+console.log(oddOrEven(5))
